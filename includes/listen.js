b88feccbfa3140b5e9c56225e093a5668f1860a8
alikhan250811-bit <alikhan250811@gmail.com>
2025-07-31 14:19:44 +0500

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -41,7 +41,7 @@ module.exports = function ({ api, models }) {
                             return a.name.localeCompare(b.name);
                         }
                     });
-                    let checkttBody = '==PRIYANSH RAJPUT ❤️==\n\n';
+                    let checkttBody = '==꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂ ❤️==\n\n';
                     checkttBody += storage.slice(0, 10).map(item => {
                         return `${count++}. ${item.name} with ${item.count} message`;
                     }).join('\n');
@@ -78,7 +78,7 @@ module.exports = function ({ api, models }) {
                                 return a.name.localeCompare(b.name);
                             }
                         });
-                        let checkttBody = '==PRIYANSH RAJPUT ❤️==\n\n';
+                        let checkttBody = '==꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂==\n\n';
                         checkttBody += storage.slice(0, 10).map(item => {
                             return `${count++}. ${item.name} with ${item.count} message`;
                         }).join('\n');
